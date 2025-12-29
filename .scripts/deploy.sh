4c095f5072ab5c91b1f9d83ce5fc5568a98611c8
ismayil.khayredinov@holoplot.com <ismayil.khayredinov@holoplot.com>
2025-12-29 14:15:36 +0100

    Fix deploy script

--- .scripts/deploy.sh
+++ .scripts/deploy.sh
@@ -12,14 +12,17 @@ cd dist
 # if you are deploying to a custom domain
 # echo 'www.example.com' > CNAME
 
+# remove existing git folder to start fresh
+rm -rf .git
+
 git init
 git add -A
 git commit -m 'deploy'
 
-# if you are deploying to https://hypejunction.github.io
-# git push -f git@github.com:hypejunction/hypejunction.github.io.git master
+# if you are deploying to https://hypeJunction.github.io
+# git push -f git@github.com:hypeJunction/hypeJunction.github.io.git master
 
-# if you are deploying to https://hypejunction.github.io/<REPO>
-git push -f git@github.com:hypejunction/color-wizard.git master:gh-pages
+# if you are deploying to https://hypeJunction.github.io/<REPO>
+git push -f git@github.com:hypeJunction/color-wizard.git main:gh-pages
 
 cd -
